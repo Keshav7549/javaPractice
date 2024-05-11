fc972131cd37377ba92f9296d1b03e9a97426ca6
Keshav <Keshav7549@gmail.com>
2024-05-12 02:41:05 +0530

    Java Code practice for star patterns 1

---
+++ src/main/code/StarPattern.java
@@ -0,0 +1,27 @@
+package src.main.code;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        int n=5;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if( j<=i ){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*    OUTPUT
+
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
+ */
